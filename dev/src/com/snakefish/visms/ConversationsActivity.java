1b680226df40d79e13d54b53ff4c40a1091d3ad4
Jim Dudish <jmd2188@rit.edu>
2010-10-03 13:15:34 -0400

    ConversationsActivity about halfway done.

---
+++ dev/src/com/snakefish/visms/ConversationsActivity.java
@@ -0,0 +1,25 @@
+package com.snakefish.visms;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ConversationsActivity extends ListActivity {
+	public static final int COMPOSE_POSITION = Menu.FIRST;
+	public static final int SETTINGS_POSITION = Menu.FIRST + 1;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.convo_list);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        boolean result = super.onCreateOptionsMenu(menu);
+        menu.add(0, COMPOSE_POSITION, 0, R.string.compose);
+        menu.add(0, Menu.FIRST, 0, R.string.settings);
+        return result;
+    }
+}
