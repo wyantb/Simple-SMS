bac8e1273337ce449933d5f8776644e7af61666e
Jason Buoni (jlb9251 <Jason Buoni (jlb9251@rit.edu)>
2010-10-05 11:34:37 -0400

    Added a little more and got rid of all the un-needed code

--- dev/src/com/snakefish/visms/IncomingMessage.java
+++ dev/src/com/snakefish/visms/IncomingMessage.java
@@ -20,27 +20,10 @@ public class IncomingMessage extends Activity {
 	public static final int SETTINGS_POSITION = Menu.FIRST + 1;
 	/** The button used for viewing the message */
 	Button btnRead;
+	/** The button used to ignore messages */
+	Button btnIgnore;
 	/** Used to get the name of the contact */
     public String name = new String();
-    
-    /** 
-     * The constructor 
-     */
-    public IncomingMessage(){
-    	
-    }
-    
-    /**
-     *  The constructor. Allows to store a name 
-     */
-    public IncomingMessage(String name){
-    	//Sets name to incoming name
-    	this.name = name;
-    }
-    
-	public void buildGUI(String name){
-		
-	}
 	
     /** Called when the activity is first created. */
     @Override
@@ -49,23 +32,9 @@ public class IncomingMessage extends Activity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.main);        
  
-        //Sets btnRead to the button on the android xml
+        //Sets btnRead and btnIgnore to the button on the android xml
         btnRead = (Button) findViewById(R.id.btnRead);
-        
-        /*btnRead.setOnClickListener(new View.OnClickListener() 
-        {
-            public void onClick(View v) 
-            {                
-                String phoneNo = txtPhoneNo.getText().toString();
-                String message = txtMessage.getText().toString();                 
-                if (phoneNo.length()>0 && message.length()>0)                
-                    sendSMS(phoneNo, message);                
-                else
-                    Toast.makeText(getBaseContext(), 
-                        "Please enter both phone number and message.", 
-                        Toast.LENGTH_SHORT).show();
-            }
-        }); */        
+        btnIgnore = (Button) findViewById(R.id.btnIgnore);        
     }    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
