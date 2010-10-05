980ef1b156986bb9f2a9e0ff4c6a094881bdf4d0
Jason Buoni (jlb9251 <Jason Buoni (jlb9251@rit.edu)>
2010-10-05 03:13:07 -0400

    A few new things for the incoming message. Java file needs some work but the xml should be good

--- dev/gen/com/snakefish/visms/R.java
+++ dev/gen/com/snakefish/visms/R.java
@@ -14,11 +14,18 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int text1=0x7f050000;
+        public static final int btnIgnore=0x7f050004;
+        public static final int btnRead=0x7f050003;
+        public static final int messageFrom=0x7f050002;
+        public static final int newText=0x7f050000;
+        public static final int text1=0x7f050005;
+        public static final int textPic=0x7f050001;
     }
     public static final class layout {
         public static final int convo_list=0x7f030000;
-        public static final int list_item=0x7f030001;
+        public static final int incoming_message=0x7f030001;
+        public static final int incoming_message_landscape=0x7f030002;
+        public static final int list_item=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
