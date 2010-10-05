e657031a8803a57cfa290b72e9e02b4f323e3be4
Jason Buoni (jlb9251 <Jason Buoni (jlb9251@rit.edu)>
2010-10-05 03:12:15 -0400

    Added a little more

--- dev/src/com/snakefish/visms/IncomingMessage.java
+++ dev/src/com/snakefish/visms/IncomingMessage.java
@@ -3,6 +3,8 @@ package com.snakefish.visms;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+
 import java.awt.*;
 
 /**
@@ -16,12 +18,10 @@ public class IncomingMessage extends Activity {
 	/**Used to positon buttons */
 	public static final int COMPOSE_POSITION = Menu.FIRST;
 	public static final int SETTINGS_POSITION = Menu.FIRST + 1;
-	
+	/** The button used for viewing the message */
+	Button btnRead;
 	/** Used to get the name of the contact */
     public String name = new String();
-    
-    /** JLable used to store name */
-    //JLabel contactName = new JLabel();
     
     /** 
      * The constructor 
@@ -42,13 +42,31 @@ public class IncomingMessage extends Activity {
 		
 	}
 	
-	/** Called when the activity is first created. */
+    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.convo_list);
-    }
-    
+        //setContentView(R.layout.main);        
+ 
+        //Sets btnRead to the button on the android xml
+        btnRead = (Button) findViewById(R.id.btnRead);
+        
+        /*btnRead.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {                
+                String phoneNo = txtPhoneNo.getText().toString();
+                String message = txtMessage.getText().toString();                 
+                if (phoneNo.length()>0 && message.length()>0)                
+                    sendSMS(phoneNo, message);                
+                else
+                    Toast.makeText(getBaseContext(), 
+                        "Please enter both phone number and message.", 
+                        Toast.LENGTH_SHORT).show();
+            }
+        }); */        
+    }    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         boolean result = super.onCreateOptionsMenu(menu);
