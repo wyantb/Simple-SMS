0cd0e6e794b797dd3e13926704387748a9c73466
Jason Buoni (jlb9251 <Jason Buoni (jlb9251@rit.edu)>
2010-10-06 15:08:50 -0400

    For when a message is sent

---
+++ dev/src/com/snakefish/visms/TestIncoming.java
@@ -0,0 +1,24 @@
+package com.snakefish.visms;
+
+/**
+ * Tests the IncomingMessage class we have 
+ * created.
+ * 
+ * @author Team Snakefish
+ *
+ */
+public class TestIncoming {
+	
+	/**
+	 * Calls the onCreate method from
+	 * IncomingMessage.java
+	 * 
+	 * @param args
+	 */
+	public static void main(String args[]){
+		//Makes a new IncomingMessage
+		IncomingMessage testIncoming = new IncomingMessage("Jason");
+		//Calls onCreate
+		//testIncoming.onCreate("incomingMessage");
+	}
+}
