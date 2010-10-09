e469bde8eb7b43570402050e0af9d0098f905353
Jason Buoni (jlb9251 <Jason Buoni (jlb9251@rit.edu)>
2010-10-09 02:40:30 -0400

    Added listeners to each button (Read and ignore). The read button should jump to text activity (idk what bundle to use so it is null for now) and the ignore button will call onFinish and end the program after saying "Message Ignored". I also have an onStop() method incase the onFinish one doesn't work so I can easily change it to that.

--- dev/src/com/snakefish/visms/IncomingMessage.java
+++ dev/src/com/snakefish/visms/IncomingMessage.java
@@ -2,6 +2,8 @@ package com.snakefish.visms;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 /**
@@ -12,13 +14,13 @@ import android.widget.Button;
  *
  */
 public class IncomingMessage extends SMSActivity {
-	/**Used to positon buttons */
+	/**Used to position buttons */
 	public static final int COMPOSE_POSITION = Menu.FIRST;
 	public static final int SETTINGS_POSITION = Menu.FIRST + 1;
 	/** The button used for viewing the message */
-	protected Button btnRead;
+	protected Button btnRead = null;
 	/** The button used to ignore messages */
-	protected Button btnIgnore;
+	protected Button btnIgnore = null;
 	/** Used to get the name of the contact */
 	protected String name = new String();
 	/** Used to determine orientation of the phone */
@@ -52,7 +54,11 @@ public class IncomingMessage extends SMSActivity {
         }
         //Sets btnRead and btnIgnore to the button on the android xml
         btnRead = (Button) findViewById(R.id.btnRead);
-        btnIgnore = (Button) findViewById(R.id.btnIgnore);        
+        btnIgnore = (Button) findViewById(R.id.btnIgnore);   
+        
+        //Sets listeners to each button
+        btnRead.setOnClickListener(new ReadListener());
+        btnIgnore.setOnClickListener(new IgnoreListener());
     }    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -83,5 +89,80 @@ public class IncomingMessage extends SMSActivity {
      */
     public void setView(boolean landscape){
     	this.landscape = landscape;
+    }
+    
+    /**
+     * This class will be used if the incoming message
+     * is ignored. The program will speak "Message
+     * Ignored" and close gracefully.
+     */
+    public void onFinish(){
+    	//Lets the user know the message has been ignored
+    	speak("Message Ignored");
+    }
+    
+    /**
+     * The stopping method used for the program. 
+     * I really don't know if we need to use this or 
+     * not.
+     */
+    public void onStop(){
+    	super.onStop();
+    }
+    
+    public void onDestroy(){
+    	super.onDestroy();
+    }
+    
+    /**
+     * The listener used for reading messages. This class
+     * will be used to change the screen to the message
+     * screen.
+     */
+    private class ReadListener implements OnClickListener {
+
+    	/**
+    	 * Use to call the work method. Similar to the 
+    	 * implementation in TextActivity.java
+    	 */
+		public void onClick(View arg0) {
+			// Calls the work method
+			doWork();
+		}
+    	
+		/**
+		 * Switches to the next screen
+		 */
+		public void doWork(){
+			//Makes a new TextActivity
+			TextActivity viewText = new TextActivity();
+			//Creates the TextActivity
+			viewText.onCreate(null);
+		}
+    }
+    
+    /**
+     * The listener used for ignoring messages. It will call 
+     * the doWork method which will call onFinish.
+     */
+    private class IgnoreListener implements OnClickListener {
+
+    	/**
+    	 * Use to call the work method. Similar to the 
+    	 * implementation in TextActivity.java
+    	 */
+		public void onClick(View arg0) {
+			// Calls the work method
+			doWork();
+		}
+    	
+		/**
+		 * Calls the onFinish method to end
+		 * the application.
+		 */
+		public void doWork(){
+			//Calls onFinish
+			onFinish();
+		}
     }
 }
